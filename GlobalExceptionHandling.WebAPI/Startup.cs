83236830c82020817522b46e8175633b92dac820
Fernando Calmet <fercalmet@gmail.com>
2021-11-23 18:43:21 -0500

    first commit

---
+++ GlobalExceptionHandling.WebAPI/Startup.cs
@@ -0,0 +1,71 @@
+using GlobalExceptionHandling.WebAPI.Handlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace GlobalExceptionHandling.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GlobalExceptionHandling.WebAPI", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GlobalExceptionHandling.WebAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+            //app.UseExceptionHandler(
+            //    options =>
+            //    {
+            //        options.Run(
+            //            async context =>
+            //            {
+            //                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            //                context.Response.ContentType = "text/html";
+            //                var exceptionObject = context.Features.Get<IExceptionHandlerFeature>();
+            //                if (null != exceptionObject)
+            //                {
+            //                    var errorMessage = $"{exceptionObject.Error.Message}";
+            //                    await context.Response.WriteAsync(errorMessage).ConfigureAwait(false);
+            //                }
+            //            });
+            //    }
+            //);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# CS-ASPNET-Core-Global-Exception-Handling
-🦄 Este proyecto es una muestra de una aplicación...
+# 🦄 C# ASP.NET CORE 5 WEB API GLOBAL EXCEPTION HANDLING
+
+[![Github][github-shield]][github-url]
+[![Kofi][kofi-shield]][kofi-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+[![Khanakat][khanakat-shield]][khanakat-url]
+
+## TABLA DE CONTENIDO
+
+* [Acerca del proyecto](#acerca-del-proyecto)
+* [Características](#características)
+* [Instalación](#instalación)
+* [Dependencias](#dependencias)
+* [Licencia](#licencia)
+
+## 🔥 ACERCA DEL PROYECTO
+
+Este proyecto es una muestra de una aplicación web API que implementa el manejo de excepciones globales. Se utilizo ``ASP.NET Core 5 Web API`` con C#.
+
+## ✔️ CARACTERÍSTICAS
+
+- [x] Getting started with Exception Handling
+- [x] Default Exception Handling Middleware
+- [x] Custom Middleware – Global Exception Handling
+
+## ⚙️ INSTALACIÓN
+
+Clonar el repositorio.
+
+```bash
+gh repo clone FernandoCalmet/CS-ASPNET-Core-Global-Exception-Handling
+```
+
+Ejecutar aplicación.
+
+```bash
+dotnet run
+```
+
+## 📥 DEPENDENCIAS
+
+- [Swashbuckle.AspNetCore](https://www.nuget.org/packages/Swashbuckle.AspNetCore/) : Herramientas Swagger para documentar API creadas en ASP.NET Core.
+
+## 📄 LICENCIA
+
+Este proyecto está bajo la Licencia (Licencia MIT) - mire el archivo [LICENSE](LICENSE) para más detalles.
+
+## ⭐️ DAME UNA ESTRELLA
+
+Si esta Implementación le resultó útil o la utilizó en sus Proyectos, déle una estrella. ¡Gracias! O, si te sientes realmente generoso, [¡Apoye el proyecto con una pequeña contribución!](https://ko-fi.com/fernandocalmet).
+
+<!--- reference style links --->
+[github-shield]: https://img.shields.io/badge/-@fernandocalmet-%23181717?style=flat-square&logo=github
+[github-url]: https://github.com/fernandocalmet
+[kofi-shield]: https://img.shields.io/badge/-@fernandocalmet-%231DA1F2?style=flat-square&logo=kofi&logoColor=ff5f5f
+[kofi-url]: https://ko-fi.com/fernandocalmet
+[linkedin-shield]: https://img.shields.io/badge/-fernandocalmet-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/fernandocalmet
+[linkedin-url]: https://www.linkedin.com/in/fernandocalmet
+[khanakat-shield]: https://img.shields.io/badge/khanakat.com-brightgreen?style=flat-square
+[khanakat-url]: https://khanakat.com
